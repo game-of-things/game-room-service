a61bd7246dd6f97c96c5eeacb4008d1d04a80e4e
Shane Rowley <srowley@redhat.com>
2020-03-08 13:37:16 -0400

    envvars for curl commands

--- test/curl.sh
+++ test/curl.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-curl -X POST -s -H "Content-type: application/json" -d '{"name":"shane"}' localhost:8080/rooms/create
+#HOST_NAME=http://localhost:8080
+HOST_NAME=http://game-room-gameofthings.apps-crc.testing:80
 
-curl -X GET -s -H "Content-type: application/json" localhost:8080/room/$ROOM_CODE
+curl -X POST -s -H "Content-type: application/json" -d '{"name":"shane"}' $HOST_NAME/rooms/create
 
-curl -X GET -s -H "Content-type: application/json" localhost:8080/rooms
+curl -X GET -s -H "Content-type: application/json" $HOST_NAME/room/$ROOM_CODE
 
-curl -X POST -s -H "Content-type: application/json" -d '{"name": "chelsea"}' localhost:8080/room/$ROOM_CODE/join
+curl -X GET -s -H "Content-type: application/json" $HOST_NAME/rooms
 
-curl -X POST -s -H "Content-type: application/json" -d '{"name": "chelsea"}' localhost:8080/room/$ROOM_CODE/quit
+curl -X POST -s -H "Content-type: application/json" -d '{"name": "matt"}' $HOST_NAME/room/$ROOM_CODE/join
+
+curl -X POST -s -H "Content-type: application/json" -d '{"name": "matt"}' $HOST_NAME/room/$ROOM_CODE/quit
